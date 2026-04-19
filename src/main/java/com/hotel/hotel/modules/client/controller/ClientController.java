8c330daf365929a2cb318a6ab43772fdfb6e6103
Peterson William <petersondev.profissional@gmail.com>
2026-04-19 12:11:11 -0300

    update

--- src/main/java/com/hotel/hotel/modules/client/controller/ClientController.java
+++ src/main/java/com/hotel/hotel/modules/client/controller/ClientController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
