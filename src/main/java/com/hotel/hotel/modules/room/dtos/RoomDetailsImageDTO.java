cc4f239093d5f5b6e64b205b330a6867c15e08a6
Peterson William <petersondev.profissional@gmail.com>
2026-05-26 19:36:59 -0300

    feat: melhorando lógica de gerenciamento de quartos

--- src/main/java/com/hotel/hotel/modules/room/dtos/RoomDetailsImageDTO.java
+++ src/main/java/com/hotel/hotel/modules/room/dtos/RoomDetailsImageDTO.java
@@ -1,24 +1,27 @@
 package com.hotel.hotel.modules.room.dtos;
 
 import java.math.BigDecimal;
+import java.util.List;
 
+import com.hotel.hotel.modules.files.dto.FileResponse;
 import com.hotel.hotel.modules.room.model.Category;
 import com.hotel.hotel.modules.room.model.Room;
 import com.hotel.hotel.modules.room.model.StatusRoom;
 
-public record RoomDetailsDTO(
+public record RoomDetailsImageDTO(
         Long id,
         String code,
         String floor,
         BigDecimal customPrice,
         Boolean active,
-        StatusRoom statusRoom,
+        StatusRoom status,
         String bedconfig,
         String amenities,
         Integer capacity,
-        Category category
+        Category category,
+        List<FileResponse> images
 ) {
-    public RoomDetailsDTO(Room room) {
+    public RoomDetailsImageDTO(Room room, List<FileResponse> images) {
         this(
                 room.getId(),
                 room.getCode(),
@@ -29,7 +32,8 @@ public record RoomDetailsDTO(
                 room.getBedConfig(),
                 room.getAmenities(),
                 room.getCapacity(),
-                room.getCategory()
+                room.getCategory(),
+                images
         );
     }
 }
